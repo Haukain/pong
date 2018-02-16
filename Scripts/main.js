07d57f970c5ca26e7ee085af28228e750f384c99
titaniumbaryum <titouan.baillon@gmail.com>
2018-02-16 13:44:11 +0100

    button eventified

--- Scripts/main.js
+++ Scripts/main.js
@@ -10,7 +10,7 @@ let ctx = canvas.getContext("2d");
 let decalage = 250;
 var pause = false;
 var var_boucle;
-//Fonction rand() dans intervalle
+//Fonction rand() dans un intervalle
 function intervalle(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -20,18 +20,20 @@ function pause_game(){
   pause = !pause;
 	if (pause) console.log("Le jeu a été mis en pause");
 	else console.log("Le jeu a été relancé");
-  
 }
+document.getElementById("bouton_pause").addEventListener("click",pause_game,false);
 //Fonction Bouton Reset
 function reset(){
 	location.reload();
 }
+document.getElementById("bouton_reset").addEventListener("click",reset,false);
 //Fontion Bouton Couleur
 function couleur_aleatoire(){
 	pong._mobiles.forEach(function(element) {
 		element._couleur.set_couleur(Math.trunc(intervalle(0,255)),Math.trunc(intervalle(0,255)),Math.trunc(intervalle(0,255)));
 	});
 }
+document.getElementById("bouton_couleur").addEventListener("click",couleur_aleatoire,false);
 //Fonction Bouton Vitesse
 function vitesse_aleatoire(){
 	pong._mobiles.forEach(function(element) {
@@ -39,20 +41,18 @@ function vitesse_aleatoire(){
 		element.set_vy(intervalle(-6,6));
 	});
 }
+document.getElementById("bouton_vitesse").addEventListener("click",vitesse_aleatoire,false);
 //Fonction Input Nombre mobiles
 function nombre_mobiles(){
 	var nb_mobiles = parseInt(document.getElementById("nb_mobiles").value);
-	if(nb_mobiles>=1 && nb_mobiles<=250){
-		pong.reset_mobiles(nb_mobiles);
-	}
+	if(nb_mobiles<1 && nb_mobiles>250) return;
+  pong.reset_mobiles(nb_mobiles);
 }
 //Fonction Input Nombre murs
 function nombre_murs(){
 	var nb_murs = parseInt(document.getElementById("nb_murs").value);
-	if(nb_murs>=0 && nb_murs<=5){
-		pong.reset_murs(nb_murs);
-		
-	}
+  if(nb_murs<0 && nb_murs>5) return;
+  pong.reset_murs(nb_murs);
 }
 
 //Création du jeu
