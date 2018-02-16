24fd533adb07ab964177515e2fcd9ef734f2e8ee
Haukain <Haukain@users.noreply.github.com>
2018-02-16 13:41:03 +0100

    fix dessine()
    + this._ctx
    + dessin avec translate() dans mur.js

--- Scripts/triangle.js
+++ Scripts/triangle.js
@@ -2,16 +2,16 @@ class Triangle extends Mobile {
 
 	//Methodes
 	dessine() {
-	ctx.save();
-	ctx.beginPath();
-	ctx.translate(this._x,this._y);
-	ctx.rotate(this._orientation);
-	ctx.moveTo(0,this._hauteur/2);
-	ctx.lineTo(this._largeur/2,-this._hauteur/2);
-	ctx.lineTo(-this._largeur/2,-this._hauteur/2);
-	ctx.lineTo(0,this._hauteur/2);
-	ctx.fillStyle = 'rgb(' + this._couleur.get_rouge() + ',' + this._couleur.get_vert() + ',' + this._couleur.get_bleu() + ')';
-	ctx.fill();
-	ctx.restore();
+	this._ctx.save();
+	this._ctx.beginPath();
+	this._ctx.translate(this._x,this._y);
+	this._ctx.rotate(this._orientation);
+	this._ctx.moveTo(0,this._hauteur/2);
+	this._ctx.lineTo(this._largeur/2,-this._hauteur/2);
+	this._ctx.lineTo(-this._largeur/2,-this._hauteur/2);
+	this._ctx.lineTo(0,this._hauteur/2);
+	this._ctx.fillStyle = 'rgb(' + this._couleur.get_rouge() + ',' + this._couleur.get_vert() + ',' + this._couleur.get_bleu() + ')';
+	this._ctx.fill();
+	this._ctx.restore();
   }
 }
